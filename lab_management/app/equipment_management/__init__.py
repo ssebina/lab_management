6f4af7ffed79eaf2916d7105908b06362036f3e6
Ssebina Abdul <as0703353316@gmail.com>
2022-04-26 05:43:55 +0300

    ignoreing unwanted files

--- lab_management/app/equipment_management/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('equipment_management', __name__)
-
-from . import routes
--- lab_management/app/hospital_management/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('hospital_management', __name__)
-
-
-from . import routes
